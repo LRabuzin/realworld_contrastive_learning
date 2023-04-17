cd4591b4266da0fd440a6b7c311ee6a168a0052f
LRabuzin <lovro.rabuzin@gmail.com>
2023-04-17 17:44:14 +0200

    change batch size

--- projLib/main.py
+++ projLib/main.py
@@ -260,7 +260,7 @@ def main():
         intermediates = []
         raw_predictions = {}
         for category in content_categories:
-            mlpreg = MLPClassifier(max_iter=1000)
+            mlpreg = MLPClassifier(max_iter=1000, batch_size=args.batch_size)
             acc_mlp, raw_prediction = evaluate_prediction(mlpreg, accuracy_score, data[0], data[1][category], data[2], data[3][category])
             intermediates.append(acc_mlp)
             raw_predictions[category] = [int(prediction) for prediction in raw_prediction]
